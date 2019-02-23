725c7ad0706972343fe4878108dccf056a2608f8
ryan p <ryan.eliopoulos@gmail.com>
2019-02-22 23:29:43 -0800

    single pipe works

--- uconnect.c
+++ uconnect.c
@@ -1,3 +1,14 @@
+
+#define ARG_LEN_MAX 1000
+
+/* Error codes */
+#define PIPE_ERR 1
+#define EXEC_ERR 2
+#define DUP_ERR 3
+#define READ_ERR 4
+
+
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<unistd.h>
@@ -8,6 +19,11 @@
 int nextArg(char *[], int);
 
 void main(int argc, char*argv[]) {
+
+    if (argc == 1) {
+        fprintf(stderr, "Need more args\n");
+        exit(1);
+    }
 
     char **parent_argv;    
 
@@ -21,6 +37,7 @@ void main(int argc, char*argv[]) {
         printf("exec time\n");
             if (execvp(*parent_argv, parent_argv)) {
                 fprintf(stderr, "error occurred\n%s\n", strerror(errno));
+                exit(EXEC_ERR);
             }
     }
     printf("next arg is %d\n", nxt_arg);    
@@ -28,18 +45,66 @@ void main(int argc, char*argv[]) {
     if (!nxt_arg) fprintf(stderr, ": is not a valid command\n");
 
     /* there is at least one :, so forking begins */
-    char **child_argv = malloc (sizeof (char *) * argc);
+    
+    /* need to save stdout for the final parent */
+    int fout; 
+    if ((fout = dup2(2, 9)) == -1) {
+        fprintf(stderr, "dup2 failed\n%s\n", strerror(errno));
+        exit(DUP_ERR);
+    }
 
+    /* set file descriptors properly */
+    close(0);
+    close(1);    
+
+    int fd[2];
+    int rdr, wtr;
+
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "error opening pipe\n%s\n", strerror(errno));
+        exit(PIPE_ERR);
+    }
+    
+    rdr = fd[0];
+    wtr = fd[1];
+
+    /* demarcate child args */
+    char **child_argv = malloc (sizeof (char *) * argc);
     child_argv = parent_argv; /* marking arg list beginning */
     child_argv[nxt_arg] = (char *)0; /* terminate args */
 
+    /* initial fork. Parent argv will be updated afterward */  
+    if (fork()) {
 
+        /* now we determine if we begin a fork loop */
+        /* 
+        /* just for debuggin now */
+        close(wtr);
+        char word[10]; 
+        printf("in parent, can we hear child?\n");
+        while(read(rdr, word, 1)) {
+            printf("%s", word);
+        }
+    }
+    
+    /* child (exec morphs) */
+    else {
+        close(rdr);
+        execvp(*child_argv, child_argv);
+    }
     /* this is just debug now */
     //if (execvp(*child_argv, child_argv) == -1) fprintf(stderr, "second exec failed\n%s\n", strerror(errno));
 
 
 }
 
+int readWrapper(int fd, char *buf, int count) {
+
+    if (read(fd, buf, count) == -1) {
+        fprintf(stderr, "read error\n%s\n", strerror(errno));
+        exit(READ_ERR);
+    }
+}
 
 /* returns offset of next : separator */
 /* if 0 is returned the caller should adjust  */
@@ -53,3 +118,7 @@ int nextArg(char* parent_argv[], int argc) {
     }
     return -1; // means no more : in argv
 }
+
+
+
+
