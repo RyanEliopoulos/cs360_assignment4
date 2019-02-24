76d226d683ef20325eef97bb4f59c4b847b62630
ryan p <ryan.eliopoulos@gmail.com>
2019-02-24 00:48:51 -0800

    trailing colon fixed

--- uconnect.c
+++ uconnect.c
@@ -26,21 +26,129 @@ int nextArg(int, char*[]);
 /* screens for syntax errors */
 /* can't begin or end with : */
 /* consecutive : are forbidden */
-void screenArgs(int, char*[]);
+/* returns 1 if trailing pipe  */
+/* present else returns 0 */
+int screenArgs(int, char*[]);
+
+
+/* magic happens here */
+void connect(int, char*[]);
 
 void execvpWrapper(char *, char*[]);
 
 void main(int argc, char*argv[]) {
-   
-    /* check syntax of command line arguments */ 
-    screenArgs(argc, argv);
-    /* at least one command line arg to process */
+
+    /* fixed main */   
+    /* to deal with trailing colons */
+    
+    /* if argc == 1 fprintf "insufficient arguments" */
+    /* if lead with colon fprintf ": cannot be first argument: */ 
+    /* if two colons in a row fprintf("Can't have two consecutive ':' arguments" */
+
+    /* if trailingColon(argc, argv) {                       */
+    /*      create new_argv to hold all but the last string */
+    /*      decrement argc                                  */
+
+    /* connect(argc, argv)                                  */
+
+    /* trailing pipe present */
+    /* copy all but last argv into a new array */
+    if (screenArgs(argc, argv)) {
+        printf("Trailing pipe present\n");
+        char *new_argv[argc];
+        for (int i = 0; i < (argc - 1); i++) new_argv[i] = argv[i];
+        new_argv[argc-1] = (char *)0;
+        
+        connect(--argc, new_argv);
+    }
+    /* no trailing pipe present */
+    /* no further adjustments needed */
+    connect(argc, argv);
+    
+    /* DONE */
+    printf("never Here, right?\n");
+}
+
+
+/* checks argv for any syntax violations */
+/* can't start or end with a : */
+/* there can't be two : in a row */
+/* returns 1 if trailing pipe, else 0 */
+int screenArgs(int argc, char*argv[]) {
+
+    /* check for args */
+    if (argc == 1) {
+        fprintf(stderr, "Need at least one argument\n");
+        exit(ARG_ERR);
+    }
+
+    /* check args don't begin with : */
+    if (!strcmp(":", argv[1])) {
+        fprintf(stderr, "Arguments cannot being with a ':'\n");
+        exit(ARG_ERR);
+    }
+    
+    /* now check for consecutive : */  
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(":", argv[i]) && !strcmp(argv[i], argv[i-1])) {
+            fprintf(stderr, "Cannot have consecutive ':' symbols\n");
+            fprintf(stderr, "Check symbol at position %d\n", i-1);
+            exit(ARG_ERR);
+        } 
+    }
+
+    /* check for trailing pipe */
+    if (!strcmp(":", argv[argc-1])) {
+        return 1;
+        //fprintf(stderr, "Arguments cannot end with a ':'\n");
+        //exit(ARG_ERR);
+    }  
+    return 0;
+}
+
+int readWrapper(int fd, char *buf, int count) {
+
+    int red;
+    if ( (red = read(fd, buf, count)) == -1) {
+        fprintf(stderr, "read error\n%s\n", strerror(errno));
+        exit(READ_ERR);
+    }
+    return red;
+}
+
+/* returns offset of next : separator */
+/* or 0 if there isn't one */
+int nextArg(int argc, char* parent_argv[]) {
+    dprintf(9, "in nxt arg\n");
+    dprintf(9, "argc is %d\n", argc);
+    dprintf(9, "parent_argv is %s\n", *parent_argv); 
+
+    for (int i = 1; i < argc; i++) { /* p_argv[0] will never be a colon */
+        if (!strcmp(":", *(parent_argv + i))) {
+            dprintf(9, "nextArg is returning %d\n", i);
+            return i;
+        }
+    }
+    dprintf(9, "nextArg is returning 0\n");
+    return 0;
+    /*
+    int i = 0;
+    while (parent_argv[i] != NULL) {
+        if (!strcmp(":", parent_argv[i])) return i; 
+        i++;
+    }
+    return -1; // means no more : in argv
+    */
+}
+
+
+void connect(int argc, char*argv[]) {
 
     /* index argv to track argument processing */ 
     char **parent_argv;    
     parent_argv = argv + 1; // first argument not itself
     argc--;  //  argc tracks parent_argv now
-
+    
 
     /* first check if there are any pipes at all*/
     int pipe_index;
@@ -192,74 +300,6 @@ void main(int argc, char*argv[]) {
     }
 }
 
-
-/* checks argv for any syntax violations */
-/* can't start or end with a : */
-/* there can't be two : in a row */
-void screenArgs(int argc, char*argv[]) {
-
-    /* check for args */
-    if (argc == 1) {
-        fprintf(stderr, "Need at least one argument\n");
-        exit(ARG_ERR);
-    }
-
-    /* check args don't begin with : */
-    if (!strcmp(":", argv[1])) {
-        fprintf(stderr, "Arguments cannot being with a ':'\n");
-        exit(ARG_ERR);
-    }
-
-    /* check args dont end with : */
-    if (!strcmp(":", argv[argc-1])) {
-        fprintf(stderr, "Arguments cannot end with a ':'\n");
-        exit(ARG_ERR);
-    }  
-  
-    /* now check for consecutive : */  
-    for (int i = 1; i < argc; i++) {
-        if (!strcmp(":", argv[i]) && !strcmp(argv[i], argv[i-1])) {
-            fprintf(stderr, "Cannot have consecutive ':' symbols\n");
-            fprintf(stderr, "Check symbol at position %d\n", i-1);
-            exit(ARG_ERR);
-        } 
-    }
-}
-
-int readWrapper(int fd, char *buf, int count) {
-
-    int red;
-    if ( (red = read(fd, buf, count)) == -1) {
-        fprintf(stderr, "read error\n%s\n", strerror(errno));
-        exit(READ_ERR);
-    }
-    return red;
-}
-
-/* returns offset of next : separator */
-/* or 0 if there isn't one */
-int nextArg(int argc, char* parent_argv[]) {
-    dprintf(9, "in nxt arg\n");
-    dprintf(9, "argc is %d\n", argc);
-    dprintf(9, "parent_argv is %s\n", *parent_argv); 
-
-    for (int i = 1; i < argc; i++) { /* p_argv[0] will never be a colon */
-        if (!strcmp(":", *(parent_argv + i))) {
-            dprintf(9, "nextArg is returning %d\n", i);
-            return i;
-        }
-    }
-    dprintf(9, "nextArg is returning 0\n");
-    return 0;
-    /*
-    int i = 0;
-    while (parent_argv[i] != NULL) {
-        if (!strcmp(":", parent_argv[i])) return i; 
-        i++;
-    }
-    return -1; // means no more : in argv
-    */
-}
 
 void populateChildArgs(char *child_argv[], char **parent_argv, int index) {
 
