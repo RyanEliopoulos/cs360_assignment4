0d00a389f349711b60e86e46de990a0c5a61bdbd
ryan p <ryan.eliopoulos@gmail.com>
2019-02-24 22:59:26 -0800

    formatting/polishing

--- uconnect.c
+++ uconnect.c
@@ -1,5 +1,14 @@
+/*
+ *  Ryan Paulos
+ *  CS 360, Assignment 4
+ *  Spring 2019 
+ *
+ *
+ *
+ *
+ */
 
-#define ARG_LEN_MAX 1000
+
 
 /* Error codes */
 #define PIPE_ERR 1   /* pipe() failure */
@@ -15,65 +24,64 @@
 #include<string.h>
 #include<errno.h>
 
+/* point char*[] elements to first int */
+/* elements in char **                 */
+/* punctuates char*[] with (char *)0   */
 void populateChildArgs(char*[], char**, int);
-void argPrinter(char *[]);
-int readWrapper(int, char*, int);
 
-void pipeWrapper(int[]);
-/* finds the offset from char*[] of the next colon */
+/* returns offset of next pipe  */
+/* or 0 if there isn't one      */
 int nextArg(int, char*[]);
 
-/* screens for syntax errors */
-/* can't begin or end with : */
-/* consecutive : are forbidden */
-/* returns 1 if trailing pipe  */
-/* present else returns 0 */
+/* screens for syntax errors             */
+/* can't start with a pipe               */
+/* there can't be two pipes in a row     */
+
+/* trailing pipes are ignored, as long   */
+/* as it doesn't violate the above rules */    
+
+/* returns 1 if trailing pipe, else 0    */
 int screenArgs(int, char*[]);
 
-
-/* magic happens here */
+/* magic happens here                            */
+/* handles all exec calls, pipes, and fork logic */
 void connect(int, char*[]);
 
+
+
+/* wrappers */
 void execvpWrapper(char *, char*[]);
+void argPrinter(char *[]);
+int readWrapper(int, char*, int);
+void pipeWrapper(int[]);
+
+
+
+/**********   Program Begins   *********/
 
 void main(int argc, char*argv[]) {
 
-    /* fixed main */   
-    /* to deal with trailing colons */
-    
-    /* if argc == 1 fprintf "insufficient arguments" */
-    /* if lead with colon fprintf ": cannot be first argument: */ 
-    /* if two colons in a row fprintf("Can't have two consecutive ':' arguments" */
-
-    /* if trailingColon(argc, argv) {                       */
-    /*      create new_argv to hold all but the last string */
-    /*      decrement argc                                  */
-
-    /* connect(argc, argv)                                  */
-
-    /* trailing pipe present */
-    /* copy all but last argv into a new array */
+    /* check for a trailing pipe */
+    /* screenArgs returns 1 when */
+    /* pipe is present */
     if (screenArgs(argc, argv)) {
-        printf("Trailing pipe present\n");
+
+        /* omit the trailing pipe in a new array */
         char *new_argv[argc];
         for (int i = 0; i < (argc - 1); i++) new_argv[i] = argv[i];
         new_argv[argc-1] = (char *)0;
-        
+    
+        /* begin piping */
         connect(--argc, new_argv);
     }
+
     /* no trailing pipe present */
-    /* no further adjustments needed */
+    /* no adjustments needed */
+    /* begin piping */
     connect(argc, argv);
-    
-    /* DONE */
-    printf("never Here, right?\n");
 }
 
 
-/* checks argv for any syntax violations */
-/* can't start or end with a : */
-/* there can't be two : in a row */
-/* returns 1 if trailing pipe, else 0 */
 int screenArgs(int argc, char*argv[]) {
 
     /* check for args */
@@ -84,11 +92,11 @@ int screenArgs(int argc, char*argv[]) {
 
     /* check args don't begin with : */
     if (!strcmp(":", argv[1])) {
-        fprintf(stderr, "Arguments cannot being with a ':'\n");
+        fprintf(stderr, "Arguments cannot begin with a ':'\n");
         exit(ARG_ERR);
     }
     
-    /* now check for consecutive : */  
+    /* now check for consecutive pipes  */  
     for (int i = 1; i < argc; i++) {
         if (!strcmp(":", argv[i]) && !strcmp(argv[i], argv[i-1])) {
             fprintf(stderr, "Cannot have consecutive ':' symbols\n");
@@ -100,11 +108,10 @@ int screenArgs(int argc, char*argv[]) {
     /* check for trailing pipe */
     if (!strcmp(":", argv[argc-1])) {
         return 1;
-        //fprintf(stderr, "Arguments cannot end with a ':'\n");
-        //exit(ARG_ERR);
     }  
     return 0;
 }
+
 
 int readWrapper(int fd, char *buf, int count) {
 
@@ -116,29 +123,15 @@ int readWrapper(int fd, char *buf, int count) {
     return red;
 }
 
-/* returns offset of next : separator */
-/* or 0 if there isn't one */
+
 int nextArg(int argc, char* parent_argv[]) {
-    dprintf(9, "in nxt arg\n");
-    dprintf(9, "argc is %d\n", argc);
-    dprintf(9, "parent_argv is %s\n", *parent_argv); 
 
     for (int i = 1; i < argc; i++) { /* p_argv[0] will never be a colon */
         if (!strcmp(":", *(parent_argv + i))) {
-            dprintf(9, "nextArg is returning %d\n", i);
             return i;
         }
     }
-    dprintf(9, "nextArg is returning 0\n");
     return 0;
-    /*
-    int i = 0;
-    while (parent_argv[i] != NULL) {
-        if (!strcmp(":", parent_argv[i])) return i; 
-        i++;
-    }
-    return -1; // means no more : in argv
-    */
 }
 
 
@@ -146,24 +139,18 @@ void connect(int argc, char*argv[]) {
 
     /* index argv to track argument processing */ 
     char **parent_argv;    
-    parent_argv = argv + 1; // first argument not itself
-    argc--;  //  argc tracks parent_argv now
+    parent_argv = argv + 1; 
+    argc--;  /*  argc tracks parent_argv now */
     
 
     /* first check if there are any pipes at all*/
     int pipe_index;
-    if ( (pipe_index = nextArg(argc, parent_argv)) == 0 ) {  /* true if no pipes in command list */
-        printf("dropping into the pipe\n");
+    if ( !(pipe_index = nextArg(argc, parent_argv)) ) {  /* true if no pipes in command list */
         execvpWrapper(*parent_argv, parent_argv); 
     }
 
-    printf("hello for reference\n"); 
     /* there is at least one pipe and two progs */
     /* prepare for forking */
-
-    /* find next : */
-
-    printf("next is at offset: %d\n", pipe_index);    
 
     /* need to save stdout for the final parent */
     int fout; 
@@ -171,7 +158,6 @@ void connect(int argc, char*argv[]) {
         fprintf(stderr, "dup2 failed\n%s\n", strerror(errno));
         exit(DUP_ERR);
     }
-    printf("prepping file descriptors\n");
 
     /* set file descriptors properly */
     close(0);
@@ -179,7 +165,6 @@ void connect(int argc, char*argv[]) {
 
     int fd[2];
     int rdr, wtr;
-
     pipeWrapper(fd);
     
     rdr = fd[0];
@@ -187,17 +172,8 @@ void connect(int argc, char*argv[]) {
 
     /* demarcate child args */
     char *child_argv[argc];
-    //child_argv[0] = *parent_argv; /* marking arg list beginning */
-    //child_argv[pipe_index] = (char *)0; /* terminate args */
-
-    /* testing new copy arg */
     populateChildArgs(child_argv, parent_argv, pipe_index);
-    /* end test */
 
-    /* need a "copyArgs" command to populate child_args with everything from parent */ 
-
-
-    argPrinter(child_argv); 
     /* initial fork. Parent argv will be updated afterward */  
     /* only get here if there are at least two command-line arguments */
     if (fork()) {
@@ -207,16 +183,12 @@ void connect(int argc, char*argv[]) {
         /* 1: curent writer */
         /* 2: stderr */
 
-
         /* now update parent_arg info to see if there are further pipes */
         argc = --argc - pipe_index;  /* -- because the : is being skipped */
         parent_argv += pipe_index + 1;  // +1 because : is being skipped */
 
-        dprintf(fout, "parent_argv points at %s\n", *parent_argv);
-        dprintf(fout, "Argc is %d\n parent_argv:%s\n", argc, *parent_argv); 
-
+        /* fork loop */
         while( (pipe_index = nextArg(argc, parent_argv)) > 0) {
-            // fork loop here
             close(wtr);
 
             pipeWrapper(fd);
@@ -232,13 +204,14 @@ void connect(int argc, char*argv[]) {
             
             /* need to adjust parent_arg and child_arg values */
             populateChildArgs(child_argv, parent_argv, pipe_index);
-            argPrinter(child_argv);
 
-            argc = --argc - pipe_index; 
+            /* adjust arg values */
+            /* ignoring the pipe */
+            argc = --argc - pipe_index;  
             parent_argv += pipe_index + 1;
 
-            dprintf(fout, "parent argv is %s\n", *parent_argv);
-
+            /* fork parent prepares file descriptors */
+            /* for the next loop                     */
             if (fork()) {
 
                 /* fd maintenance for next loop */
@@ -258,12 +231,13 @@ void connect(int argc, char*argv[]) {
                 /* 2: stderr */
             }
             
-            /* fork child */
+            /* fork child     */
+            /* exec to stdout */
             else {
                 close(rdr); /* this is fd 1 */
                 dup(wtr); /* moves writer to position 1 */
                 close(wtr); 
-                dprintf(fout, "make it here, eh?\n");       
+
                 /* file descriptors */  
                 /* 0: reading from last command */
                 /* 1: current writer */
@@ -274,26 +248,16 @@ void connect(int argc, char*argv[]) {
         } 
 
         /* this is where we go after the fork loop ends */
-        /* and the final program is exec'ed */
+        /* only a single program invocation remains     */
 
         close(wtr);
-        dprintf(fout, "Final chapter ????\n");
-        dprintf(fout, "parent_argv is %s\n", *parent_argv);
         dup2(fout, 1); // set fd 1 to stdout from reserved fd
         close(fout);
-        execvp(*parent_argv, parent_argv);
-        dprintf(fout, "shouldn't ever get here\n");
-         
 
-        /* this is the final forked parent printing to stdout */        
-        close(wtr);
-        char word[10]; 
-        while(readWrapper(rdr, word, 1)) {
-            write(fout, word, 1);
-        }
+        execvp(*parent_argv, parent_argv);
     }
-    
-    /* child (exec morphs) */
+   
+    /* logic of first child process */
     else {
         close(rdr);
         execvp(child_argv[0], child_argv);
@@ -324,6 +288,7 @@ void execvpWrapper(char *prog, char*prog_args[]) {
     }
 }
 
+/* debug tool */
 void argPrinter(char *args[]) {
     
     int i = 0;
